f579bdc71926053a6eaa6e628c2be18664e073fe
Violet Sirikajornkij <thitirat.sirikajornkij@kellogg.northwestern.edu>
2024-05-31 20:16:11 +0000

    2hr api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,19 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.order("id desc")
+    posts_json = []
+    for post in posts
+      user = User.find_by({"id" => post["user_id"]})
+      posts_json << {
+        "username" => user{"username"},
+        "real_name" => user["real_name"],
+        "body" => post["body"],
+        "created_at" => post["created_at"]
+      }
+    response_to do |format|
+      format.html
+      format.json { render :json => post_json}
+    end
   end
 
   def create
